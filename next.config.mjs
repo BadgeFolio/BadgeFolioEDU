4ed34509343858aaa424029cb0bdc5a2d7658483
BadgeFolio <badgefolioedu@gmail.com>
2025-04-08 16:44:52 -0400

    Configure for Render deployment

--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,7 @@ const nextConfig = {
   images: {
     domains: ['res.cloudinary.com'],
     unoptimized: true
-  },
-  output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/badgefolio' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/badgefolio/' : '',
+  }
 };
 
 export default nextConfig; 
